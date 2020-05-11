144f9fa988ded2325131c8a20d30049129974dd9
Jaime laptop <jaime@astro.su.se>
2020-05-11 09:06:15 +0200

    changed authors list as in the paper

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ extension.cython_directives = {'language_level': "3"}
 setup(
     name = 'WFA Spat',
     version = '1.0',
-    author = 'Jaime de la Cruz Rodriguez & Roberta Morosin (ISP-SU 2020)',
+    author = 'Roberta Morosin, Jaime de la Cruz Rodriguez and Gregal Vissers (ISP-SU 2020)',
     ext_modules=[extension],
     cmdclass = {'build_ext': build_ext}
 )
