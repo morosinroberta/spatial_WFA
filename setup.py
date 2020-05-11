158b1fa27fe49b4b4857090e83897f71668af7d5
Jaime laptop <jaime@astro.su.se>
2020-05-11 08:56:33 +0200

    changed setup.py to detect OS

--- setup.py
+++ setup.py
@@ -6,21 +6,33 @@ import numpy
 from distutils import sysconfig
 #import numpy.distutils.intelccompiler
 import numpy.distutils.ccompiler
+import platform as plt
 
-os.environ["CC"] = "g++"
-os.environ["CXX"] = "g++"
+if(plt.system() == 'Darwin'):
+    root_dir = '/opt/local/'
+    CC = 'clang'
+    CXX= 'clang++'
+    link_opts = ["-stdlib=libc++","-bundle","-undefined","dynamic_lookup"]
+else:
+    root_dir = '/usr/'
+    CC = 'gcc'
+    CXX= 'g++'
+    link_opts = ["-shared"]
+
+os.environ["CC"] = CC
+os.environ["CXX"] = CXX
 
 from distutils import sysconfig
 sysconfig.get_config_vars()['CFLAGS'] = ''
 sysconfig.get_config_vars()['OPT'] = ''
 sysconfig.get_config_vars()['PY_CFLAGS'] = ''
 sysconfig.get_config_vars()['PY_CORE_CFLAGS'] = ''
-sysconfig.get_config_vars()['CC'] = 'gcc'
-sysconfig.get_config_vars()['CXX'] = 'g++'
+sysconfig.get_config_vars()['CC'] =  CC
+sysconfig.get_config_vars()['CXX'] = CXX
 sysconfig.get_config_vars()['BASECFLAGS'] = ''
 sysconfig.get_config_vars()['CCSHARED'] = ''
-sysconfig.get_config_vars()['LDSHARED'] = 'g++ -shared'
-sysconfig.get_config_vars()['CPP'] = 'g++'
+sysconfig.get_config_vars()['LDSHARED'] = CC
+sysconfig.get_config_vars()['CPP'] = CXX
 sysconfig.get_config_vars()['CPPFLAGS'] = ''
 sysconfig.get_config_vars()['BLDSHARED'] = ''
 sysconfig.get_config_vars()['CONFIGURE_LDFLAGS'] = ''
@@ -30,13 +42,12 @@ sysconfig.get_config_vars()['PY_LDFLAGS'] = ''
 
 
 comp_flags=['-O3','-std=c++11','-march=native','-fPIC']#,
-root_dir = '/usr/'
 extension = Extension("spatWFA",
                       sources=["WFA.pyx"], 
                       include_dirs=["./", root_dir+"/include/",numpy.get_include()],
                       language="c++",
                       extra_compile_args=comp_flags,
-                      extra_link_args=['-shared'],
+                      extra_link_args=link_opts,
                       library_dirs=[root_dir+'/lib/','./'],
                       libraries=[])
 
